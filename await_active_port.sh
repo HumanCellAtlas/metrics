4dfe5197ba226f2cc80445f9f3c2cc4bc0d946b1
Matt Weiden <538456+mweiden@users.noreply.github.com>
2019-08-02 12:50:57 -0700

    Update await_active_port.sh

--- await_active_port.sh
+++ await_active_port.sh
@@ -5,7 +5,7 @@ ATTEMPTS=1
 MAX_ATTEMPTS=10
 
 while ! nc -z localhost "$PORT"; do
-  sleep 1.0 # wait for 1/10 of the second before check again
+  sleep 1.0 # wait before checking again
   echo "Waiting for active scan on port $PORT..."
   ATTEMPTS=$(( $ATTEMPTS + 1 ))
   if [ $ATTEMPTS -gt $MAX_ATTEMPTS ]; then
